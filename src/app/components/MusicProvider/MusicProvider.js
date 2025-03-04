afd406029716262f16cee94016e0fb8df627f4de
Adeolatus <adeola@cautioneo.com>
2025-03-04 10:10:57 +0100

    Push Howler

--- src/app/components/MusicProvider/MusicProvider.js
+++ src/app/components/MusicProvider/MusicProvider.js
@@ -1,64 +1,57 @@
-'use client'
-import { createContext, useState, useRef, useContext, useEffect } from "react";
+'use client';
+import { createContext, useState, useContext, useEffect } from 'react';
+import { Howl } from 'howler';
 
 const MusicContext = createContext();
 
 export function MusicProvider({ children }) {
-  const [isClient, setIsClient] = useState(false);
-
-  // Utilisation de useRef pour conserver l'audio
-  const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [sound, setSound] = useState(null);
 
-  // Assurez-vous que le code ne s'exécute que côté client
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const audio = new Audio("/images/musicfond.mp3");
-      audio.loop = true;
-      audioRef.current = audio;
-      setIsClient(true); // Le client est maintenant prêt
+      const music = new Howl({
+        src: ['/images/musicfond.mp3'],
+        loop: true,
+        volume: 0,
+      });
+      setSound(music);
     }
   }, []);
 
-  // Fonction pour effectuer un fade (augmentation ou diminution du volume)
   const fadeAudio = (audio, fadeTime, type) => {
-    let volume = type === "in" ? 0 : 1;
-    audio.volume = Math.min(1, Math.max(0, volume));
+    let volume = type === 'in' ? 0 : 1;
+    audio.volume(volume);
 
     const interval = setInterval(() => {
-      if (type === "in") {
+      if (type === 'in') {
         volume += 0.05;
         if (volume >= 1) {
           clearInterval(interval);
-          audio.volume = 1;
+          audio.volume(1);
         }
       } else {
         volume -= 0.05;
         if (volume <= 0) {
           clearInterval(interval);
-          audio.pause();
-          audio.volume = 0;
+          audio.stop();
         }
       }
-      audio.volume = Math.min(1, Math.max(0, volume));
+      audio.volume(Math.min(1, Math.max(0, volume)));
     }, fadeTime / 20);
   };
 
   const togglePlay = () => {
-    if (audioRef.current) {
+    if (sound) {
       if (isPlaying) {
-        fadeAudio(audioRef.current, 1000, "out"); // Fondu de sortie sur 1s
+        fadeAudio(sound, 1000, 'out');
       } else {
-        audioRef.current.play();
-        fadeAudio(audioRef.current, 1000, "in"); // Fondu d’entrée sur 1s
+        sound.play();
+        fadeAudio(sound, 1000, 'in');
       }
       setIsPlaying(!isPlaying);
     }
   };
-
-  if (!isClient) {
-    return null; // Ne pas rendre le composant tant que le client n'est pas prêt
-  }
 
   return (
     <MusicContext.Provider value={{ isPlaying, togglePlay }}>
@@ -70,3 +63,4 @@ export function MusicProvider({ children }) {
 export function useMusic() {
   return useContext(MusicContext);
 }
+
